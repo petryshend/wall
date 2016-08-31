548664b9e299dc7218f244191a299d6afa862d77
dmitry <dpetrishen@experty.com>
2016-08-31 16:14:38 +0300

    Add Record Entity

--- src/AppBundle/Controller/WallController.php
+++ src/AppBundle/Controller/WallController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Record;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class WallController extends Controller
@@ -13,6 +17,46 @@ class WallController extends Controller
      */
     public function indexAction(): Response
     {
-        return $this->render('wall.html.twig');
+        $records = $this->getDoctrine()->getRepository('AppBundle:Record')->findAll();
+
+        return $this->render(
+            'wall.html.twig',
+            [
+                'records' => $records
+            ]
+        );
+    }
+
+    /**
+     * @Route("/record/create", name="record_create", methods={"GET", "POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function createAction(Request $request): Response
+    {
+        $record = new Record();
+
+        $form = $this->createFormBuilder($record)
+            ->add('content', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Record'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $record = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($record);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render(
+            'record/create.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 }
---
+++ src/AppBundle/Entity/Record.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="record")
+ */
+class Record
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Record
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
