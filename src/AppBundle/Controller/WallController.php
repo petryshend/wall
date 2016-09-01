b26c1d9216bdc3b4ed239c331dca371b8dd04f82
petryshend <petryshend@gmail.com>
2016-09-01 22:07:01 +0300

    Add delete record button

--- src/AppBundle/Controller/WallController.php
+++ src/AppBundle/Controller/WallController.php
@@ -17,7 +17,8 @@ class WallController extends Controller
      */
     public function indexAction(): Response
     {
-        $records = $this->getDoctrine()->getRepository('AppBundle:Record')->findBy([], ['active' => 'DESC']);
+        $records = $this->getDoctrine()->getRepository('AppBundle:Record')
+            ->findBy([], ['active' => 'DESC', 'id' => 'DESC']);
 
         return $this->render(
             'wall.html.twig',
@@ -64,7 +65,7 @@ class WallController extends Controller
      *     name="record_toggle_active",
      *     methods={"POST"},
      *     requirements={"id": "\d+"}
-     *     )
+     * )
      * @param int $id
      * @return Response
      */
@@ -80,6 +81,31 @@ class WallController extends Controller
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($record);
+        $em->flush();
+
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route(
+     *     "/record/{id}/delete",
+     *     name="record_delete",
+     *     methods={"POST"},
+     *     requirements={"id": "\d+"}
+     * )
+     * @param int $id
+     * @return Response
+     */
+    public function deleteRecordAction(int $id): Response
+    {
+        $record = $this->getDoctrine()->getRepository('AppBundle:Record')->find($id);
+
+        if ($record === null) {
+            throw $this->createNotFoundException('No record found with id ' . $id);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($record);
         $em->flush();
 
         return $this->redirectToRoute('home');
