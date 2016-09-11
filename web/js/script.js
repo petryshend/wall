bef13d6d942b4150f2846f9cb42120a7ff0de7b5
petryshend <petryshend@gmail.com>
2016-09-11 18:32:28 +0300

    Prettify delete note button

--- web/js/script.js
+++ web/js/script.js
@@ -2,4 +2,10 @@ $(function() {
     $('#createRecordButton').on('click', function() {
         $('#new-record-form').submit();
     });
+
+    $('#delete-note-button').on('click', function() {
+        if (confirm('Are you sure you want to delete this note?')) {
+            $(this).closest('form').submit();
+        }
+    });
 });
