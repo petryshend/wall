87be3f33b5a6bec5b718f2f76661f22c75e28c37
petryshend <petryshend@gmail.com>
2016-09-17 11:11:34 +0300

    Fix delete note button

--- web/js/script.js
+++ web/js/script.js
@@ -3,7 +3,8 @@ $(function() {
         $('#new-record-form').submit();
     });
 
-    $('#delete-note-button').on('click', function() {
+    $('.delete-note-button').on('click', function(e) {
+        e.preventDefault();
         if (confirm('Are you sure you want to delete this note?')) {
             $(this).closest('form').submit();
         }
